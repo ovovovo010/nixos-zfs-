d21fb9c9d8bfed7cca5e121da6504579a1ef19e3
Eric <ovovovo010@proton.me>
2026-03-07 22:25:25 +0800

    fix

--- root/desktop/desktop.nix
+++ root/desktop/desktop.nix
@@ -1,12 +1,6 @@
 {pkgs, ...}: {
   services.xserver = {
     enable = true;
-    windowManager.awesome = {
-      enable = true;
-      luaModules = with pkgs.luaPackages; [
-        luarocks
-      ];
-    };
   };
   programs.hyprland.enable = true;
 }
