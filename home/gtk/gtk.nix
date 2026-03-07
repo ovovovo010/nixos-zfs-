68c12132907e6e858a0ef1a1f763a851bbd82e62
Eric <ovovovo010@proton.me>
2026-03-07 15:58:51 +0800

    update

--- home/gtk/gtk.nix
+++ home/gtk/gtk.nix
@@ -4,6 +4,16 @@
   gtk = {
     enable = true;
 
+    theme = {
+      name    = "adw-gtk3-dark";
+      package = pkgs.adw-gtk3;
+    };
+
+    iconTheme = {
+      name    = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
       gtk-xft-antialias = 1;
