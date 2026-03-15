390ace3e4854323c9121a36ac9f34451c2d9db2d
eric <ovovovo010@proton.me>
2026-03-15 12:00:54 +0800

    update

--- home/simple-wallpaper-engine/simple-wallpaper-engine.nix
+++ home/simple-wallpaper-engine/simple-wallpaper-engine.nix
@@ -1,7 +1,5 @@
 {...}: {
   programs.simple-wallpaper-engine = {
     enable = true;
-    wallpaperEnginePath = "/mnt/data/SteamLibrary/steamapps/common/wallpaper_engine";
-    workshopPath = "/mnt/data/SteamLibrary/steamapps/workshop/content/431960";
   };
 }
